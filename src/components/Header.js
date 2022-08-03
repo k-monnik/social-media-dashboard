7a101dd780dcb5b6d69ccb50dca5e9e11b175cab
Kristen Monnik <nolaviolinist@gmail.com>
2022-08-03 11:14:49 -0400

    reset header styling for desktop

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,21 +2,23 @@
 
 const Header = () => {
     return (
-        <header className="header">
-            <div className="w-full h-[235px] md:h-[244px] bg-light-100">
-                <h1 className="font-bold text-2xl">Social Media Dashboard</h1>
-                <div className="divide-y divide-solid divide-light-300 md:divide-none">
+        <div>
+            <div className="w-full h-60 md:h-64 bg-light-100">
+                <div className="md:px-12 pt-4">
+                    <h1 className="font-bold text-3xl">Social Media Dashboard</h1>
                     <p className="pt-0.5 pb-4 text-sm text-light-300 font-semibold ">
                         Total Followers: 23,004
                     </p>
-                    <div className="py-4 text-sm text-light-300 font-semibold flex justify-between md:justify-end md:gap-x-2 md:items-center md:align-top">
-                        Dark Mode
-
-                        {/* <Toggle /> */}
-                    </div>
+                    <div className="divide-y divide-solid divide-light-300 md:divide-none"></div>
                 </div>
+
+                {/* <div className="text-sm text-light-300 font-semibold flex justify-between  md:justify-end md:gap-x-2 md:pr-24">
+                    Dark Mode
+
+                    <Toggle />
+                </div> */}
             </div>
-        </header>
+        </div>
     )
 }
 
