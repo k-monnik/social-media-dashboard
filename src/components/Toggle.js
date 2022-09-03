cb7052e20eec62fbbec9cdadfba5756ffb3ba5f4
Kristen Monnik <nolaviolinist@gmail.com>
2022-09-03 00:24:34 -0400

    reverse colors of toggle bg and circle for toggle switch to match design

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -12,9 +12,9 @@ function Toggle() {
     return (
         <div>
             <div className="flex justify-end">
-                <label htmlFor="toggle-switch" className="text-sm font-bold text-medGrey pr-12">Dark Mode</label>
+                <label htmlFor="toggle-switch" className="text-sm font-bold text-medGrey dark:text-white pr-12">Dark Mode</label>
                 <div className="mr-24">
-                    <input className="cursor-pointer h-6 w-12 rounded-full appearance-none bg-toggle-grey checked:bg-toggle-green transition duration-200 relative" type="checkbox" id="toggle-switch" />
+                    <input className="cursor-pointer h-6 w-12 rounded-full appearance-none bg-toggle-green checked:bg-toggle-grey transition duration-200 relative" type="checkbox" id="toggle-switch" />
                 </div>
             </div>
         </div>
